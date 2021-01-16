725c21750d041c1d5a1cd34a1bce5bfefe46015e
Tristan Gibbs <gtristan89@gmail.com>
2021-01-16 15:37:14 -0700

    commit tree 2e41f1abb7f41fad89d9d6f70e95d04f49ce1965

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    attendees.collect{|attendee| badge_maker(attendee)}
+end
+
+def assign_rooms(attendees)
+    attendees.each_with_index.map{|name, index| "Hello, #{name}! You'll be assigned to room #{index+=1}!"}
+end
+
+def printer(name)
+    batch_badge_creator(name).each do |badge|
+        puts badge
+    end
+        assign_rooms(name).each do |assign|
+            puts assign
+    end
+end
